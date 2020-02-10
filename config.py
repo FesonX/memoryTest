f169ec1546a6006ea1ed4a2d20e55577a41337fc
FesonX <fesonx@foxmail.com>
2020-02-10 22:47:46 +0800

    feature: basic flask app with SQLAlchemy connect

---
+++ config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name:     config.py
+   Author:        FesonX
+   Email:         fesonx@foxmail.com
+   GitHub:        github.com/FesonX
+   date:          20-2-10
+-------------------------------------------------
+"""
+import os
+
+# Get root path of project
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
---
+++ src/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name:     __init__.py
+   Author:        FesonX
+   Email:         fesonx@foxmail.com
+   GitHub:        github.com/FesonX
+   date:          20-2-10
+-------------------------------------------------
+"""
+from os import urandom
+
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+from config import BASE_DIR
+
+
+def create_app() -> Flask:
+    app = Flask(__name__, template_folder=BASE_DIR + '/src/templates',
+                static_folder=BASE_DIR + '/src/static')
+    # Add your own instance config folder
+    # And your own config.py
+    # Contents should include `QLALCHEMY_DATABASE_URI` like the following description:
+    # # Default is MySQLdb, for other driver, format like `mysql{+driver_name}:uri`
+    # # For example, current project use `pymysql` as driver
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@localhost:3306/db_name'
+    app.config.from_pyfile(BASE_DIR + '/instance/config.py')
+    random_bytes = urandom(64)
+    app.secret_key = random_bytes
+
+    return app
+
+
+app = create_app()
+db = SQLAlchemy(app)
